aee0f40b90a0a5c3e918754210a2d8e0decaf782
Alexander671 <matveevalex97@yandex.ru>
2022-04-06 16:28:32 +0300

    Create mint function

--- tokens/views.py
+++ tokens/views.py
@@ -1,19 +1,21 @@
 
 
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from requests import get
+from nftjob import settings
 
 from tokens.form import TokensForm
 from .models import Tokens
 
 
-from web3 import Web3, HTTPProvider
+from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
 
 import environ
+
 
 # новый обьект модели Token, в который сохраняются данные из параметра запроса,
 # помимо этого генерируется любая рандомная строка*,
@@ -21,40 +23,74 @@ import environ
 # Любая рандомная строка  - должна состоять из букв и цифр
 # (т.е. без спецсимволов) и иметь длину в 20 знаков.
 
+from random import choice
+from string import ascii_letters
 
+# /tokens/create 
+# Метод запроса: POST
+# Это API должно создавать новый уникальный токен в блокчейне 
+# и записывать параметры запроса в БД.
 class TokensCreateView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        form = TokensForm()
-        
-        return render(request, 'tokens/create.html', {'form' : form})
-    
     def post(self, request):
         form = TokensForm(request.POST)
+        
+        # инициализация смарт контракта
+        contract_instance, block, gas_price, w3 = connect_smart_contract()
+        
+        # случайная строка
+        # реализовать проверку на уникальность?
+        unique_hash = ''.join(choice(ascii_letters + ''.join(map(str, range(0,10)))) for i in range(20))
+        
+        # выполнение транзакции и получение ответа
+        tx_hash = contract_instance.functions.mint(settings.ME, request.POST['owner'], settings.RANDOM_MEDIA).transact({'from' : settings.ME})
+
         if form.is_valid():
             
             req = form.save(commit=False)
-            req.user = request.user
+            req.unique_hash = unique_hash
+            req.tx_hash = tx_hash
             req.save()
             
         return render(request, 'tokens/create.html', {'form' : form})
 
 
+# /tokens/list
+# Метод запроса: GET
+# Это API  должно выдавать список всех обьектов модели Token
 class TokensListView(TemplateView):
     def get(self, request, *args, **kwargs):
         tokens = Tokens.objects.all()
         
         return render(request, 'tokens/list.html', {'tokens' : tokens})
 
-
-from nftjob import settings
-
+# /tokens/total_supply
+# Метод запроса: GET
+# Это API должно обращаться к контракту в блокчейне и выдавать
+# в ответе информацию о текущем Total supply токена - общем числе 
+# находящихся токенов в сети. Форма ответа - произвольная, в JSON-формате.
+# Минимальный базовый пример ответа - {"result": 10000}
 class TokensTotalSupplyView(TemplateView):
     def get(self, request, *args, **kwargs):
+        
+        contract_instance, block, gas_price, w3 = connect_smart_contract()
+        # info about smart-contract
+        name = contract_instance.functions.name().call()
+        totalSupply = contract_instance.functions.totalSupply().call()
+        info = [block,gas_price,name,totalSupply]
+        
+        return render(request, 'tokens/total_supply.html', {'info' : info, 'w3':w3})
+
+# функция пре-создания контракта
+def connect_smart_contract():
+    # connect to eth
         w3 = Web3(Web3.HTTPProvider(settings.INFURA_PROJECT_ID))
+        
+        # variables from .env file
         address_contract = settings.ADDRESS_CONTRACT
         me = settings.ME
-        abi = '[{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"ApprovalCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"ApprovalQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"ApprovalToCurrentOwner","type":"error"},{"inputs":[],"name":"ApproveToCaller","type":"error"},{"inputs":[],"name":"BalanceQueryForZeroAddress","type":"error"},{"inputs":[],"name":"MintToZeroAddress","type":"error"},{"inputs":[],"name":"MintZeroQuantity","type":"error"},{"inputs":[],"name":"OwnerIndexOutOfBounds","type":"error"},{"inputs":[],"name":"OwnerQueryForNonexistentToken","type":"error"},{"inputs":[],"name":"TokenIndexOutOfBounds","type":"error"},{"inputs":[],"name":"TransferCallerNotOwnerNorApproved","type":"error"},{"inputs":[],"name":"TransferFromIncorrectOwner","type":"error"},{"inputs":[],"name":"TransferToNonERC721ReceiverImplementer","type":"error"},{"inputs":[],"name":"TransferToZeroAddress","type":"error"},{"inputs":[],"name":"URIQueryForNonexistentToken","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"totalMinted","type":"uint256"}],"name":"Minted","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"_transferToContract","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"allowedToContract","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_newPrice","type":"uint256"}],"name":"changePrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipSale","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_to","type":"address"},{"internalType":"uint256","name":"_count","type":"uint256"}],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"saleOpen","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"setAllowToContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_tokenId","type":"uint256"},{"internalType":"bool","name":"_allow","type":"bool"}],"name":"setAllowTokenToContract","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
-
+        abi = settings.ABI
+        
+        # прослойка между сетями
         w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
         assert True is w3.isConnected()
@@ -68,6 +104,4 @@ class TokensTotalSupplyView(TemplateView):
         
         # info about smart-contract
         name = contract_instance.functions.name().call()
-        totalSupply = contract_instance.functions.totalSupply().call()
-        info = [block,gas_price,name,totalSupply]
-        return render(request, 'tokens/total_supply.html', {'info' : info, 'w3':w3})
+        return contract_instance, block, gas_price, w3
